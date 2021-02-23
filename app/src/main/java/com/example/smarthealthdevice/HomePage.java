a971b4d50ca9e47b75ebbaf2d47257669902a313
Dell <rishabh1422869singh@gmail.com>
2021-02-23 17:36:30 +0530

    ui clickable

--- app/src/main/java/com/example/smarthealthdevice/HomePage.java
+++ app/src/main/java/com/example/smarthealthdevice/HomePage.java
@@ -2,16 +2,30 @@ package com.example.smarthealthdevice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class HomePage extends AppCompatActivity {
+    ImageView profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_home_page );
         Toolbar toolbar=findViewById( R.id.toobar );
+        profile=findViewById( R.id.profile );
         setSupportActionBar( toolbar );
+        profile.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(HomePage.this,yourhealthprofile.class);
+                startActivity( i );
+                finish();
+            }
+        } );
     }
 }
