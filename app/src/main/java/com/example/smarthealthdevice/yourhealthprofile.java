4774f28b61208068240c617c9d57ccdb5144a2ed
Dell <rishabh1422869singh@gmail.com>
2021-02-26 16:52:14 +0530

    ui blood prassure

--- app/src/main/java/com/example/smarthealthdevice/yourhealthprofile.java
+++ app/src/main/java/com/example/smarthealthdevice/yourhealthprofile.java
@@ -1,24 +1,35 @@
 package com.example.smarthealthdevice;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-import com.google.android.material.tabs.TabLayout;
+import com.example.smarthealthdevice.Medical.Medical;
 
 public class yourhealthprofile extends AppCompatActivity {
+    TextView medical;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_yourhealthprofile );
-        PersonDatabase appData=PersonDatabase.getInstance( this );
-        appData.personDao().getPersonalList();
-        ProfileModel profileModel=new ProfileModel( "1234567","abc@co.in","19-05-1998","B+","UnMarried" );
+        medical=findViewById(R.id.medical);
+        medical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(yourhealthprofile.this, Medical.class);
+                startActivity(i);
+            }
+        });
+    //PersonDatabase appData=PersonDatabase.getInstance( this );
+        //appData.personDao().getPersonalList();
+       // ProfileModel profileModel=new ProfileModel( "1234567","abc@co.in","19-05-1998","B+","UnMarried" );
 
         /* *
          *  Insert and get data using Database Async way
@@ -28,7 +39,7 @@ public class yourhealthprofile extends AppCompatActivity {
             public void run() {
                 Log.e("TAG","data saved");
                 // Insert Data
-                appData.personDao().updatePerson( profileModel );
+                //appData.personDao().updatePerson( profileModel );
 
                 // Get Data
                 // AppDatabase.getInstance(context).userDao().getAllUsers();
